0b6bd7a17debc3279b178100a4cbf25d1a3d4efe
Unai Zulaika <unai.zulaika@deusto.es>
2024-10-21 12:10:06 +0200

    Added entity key/value to dictionary

--- IDEA4RC-utils/dm_codes_extraction.py
+++ IDEA4RC-utils/dm_codes_extraction.py
@@ -22,29 +22,34 @@ id_variable_term = {}
 for index, sheet_number in enumerate(tqdm(SHEETS_TO_PROCESS)):
     # read each sheet
     dataframe = pd.read_excel(xls, sheet_name=sheet_number)
-    for vname, terms in dataframe[["Variable Name (EURACAN file)", "Vocabulary"]].itertuples(index=False):
+    for vname, terms, entity in dataframe[
+        ["Variable Name (EURACAN file)", "Vocabulary", "ObjectClass"]
+    ].itertuples(index=False):
         for line in str(terms).splitlines():
             # Regular expression to match the format
-            pattern = r'^(?P<text>[^-]+) - (?P<number>\d+)$'
+            pattern = r"^(?P<text>[^-]+) - (?P<number>\d+)$"
 
             # Match the pattern
             match = re.match(pattern, line)
 
             # Check if the match was successful
             if match:
-                text = match.group('text').strip()  # Extract and strip any leading/trailing whitespace
-                number = int(match.group('number'))  # Convert the number to an integer
+                text = match.group(
+                    "text"
+                ).strip()  # Extract and strip any leading/trailing whitespace
+                number = int(match.group("number"))  # Convert the number to an integer
                 result_dict[text] = number  # Add to dictionary
                 id_variable_term[number] = {
                     "variable_name": vname,
-                    "term": text
+                    "term": text,
+                    "entity": entity,
                 }
 
 
 # Print the result
 print(result_dict)
-with open('dictionaries/term_to_code.json', 'w') as fp:
-    json_object = json.dump(result_dict, fp) 
+with open("dictionaries/term_to_code.json", "w") as fp:
+    json_object = json.dump(result_dict, fp)
 
-with open('dictionaries/code_to_term_variable.json', 'w') as fp:
-    json_object = json.dump(id_variable_term, fp) 
+with open("dictionaries/code_to_term_variable.json", "w") as fp:
+    json_object = json.dump(id_variable_term, fp)
